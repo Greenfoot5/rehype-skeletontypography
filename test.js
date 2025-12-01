1dcd5a822d973100274634ea3102a4744dc555a1
Greenfoot5 <greenfoot89@gmail.com>
2025-12-01 21:01:19 +0000

    Setup the plugin

--- test.js
+++ test.js
@@ -1,23 +1,24 @@
 import assert from 'node:assert/strict'
 import test from 'node:test'
 import {rehype} from 'rehype'
-import rehypeSlug from 'rehype-slug'
+import rehypeClasses from 'rehype-headingclasses'
 
-test('rehypeSlug', async function (t) {
+test('rehypeClasses', async function (t) {
   await t.test('should expose the public api', async function () {
-    assert.deepEqual(Object.keys(await import('rehype-slug')).sort(), [
-      'default'
-    ])
+    assert.deepEqual(
+      Object.keys(await import('rehype-headingclasses')).sort(),
+      ['default']
+    )
   })
 
   await t.test('should work', async function () {
     const file = await rehype()
       .data('settings', {fragment: true})
-      .use(rehypeSlug)
+      .use(rehypeClasses)
       .process(
         [
           '<section>',
-          '  <h1>Lorem ipsum 😪</h1>',
+          '  <h1 class="big-title">Lorem ipsum 😪</h1>',
           '  <h2>dolor—sit—amet</h2>',
           '  <h3>consectetur &amp; adipisicing</h3>',
           '  <h4>elit</h4>',
@@ -31,11 +32,11 @@ test('rehypeSlug', async function (t) {
       String(file),
       [
         '<section>',
-        '  <h1 id="lorem-ipsum-">Lorem ipsum 😪</h1>',
-        '  <h2 id="dolorsitamet">dolor—sit—amet</h2>',
-        '  <h3 id="consectetur--adipisicing">consectetur &#x26; adipisicing</h3>',
-        '  <h4 id="elit">elit</h4>',
-        '  <h5 id="elit-1">elit</h5>',
+        '  <h1 class="big-title h1">Lorem ipsum 😪</h1>',
+        '  <h2 class="h2">dolor—sit—amet</h2>',
+        '  <h3 class="h3">consectetur &#x26; adipisicing</h3>',
+        '  <h4 class="h4">elit</h4>',
+        '  <h5 class="h5">elit</h5>',
         '  <p>sed</p>',
         '</section>'
       ].join('\n')
@@ -45,11 +46,11 @@ test('rehypeSlug', async function (t) {
   await t.test('should support `options.prefix`', async function () {
     const file = await rehype()
       .data('settings', {fragment: true})
-      .use(rehypeSlug, {prefix: 'test-'})
+      .use(rehypeClasses, {prefix: 'test-'})
       .process(
         [
           '<section>',
-          '  <h1>Lorem ipsum 😪</h1>',
+          '  <h1 class="big-title">Lorem ipsum 😪</h1>',
           '  <h2>dolor—sit—amet</h2>',
           '  <h3>consectetur &amp; adipisicing</h3>',
           '  <h4>elit</h4>',
@@ -63,11 +64,11 @@ test('rehypeSlug', async function (t) {
       String(file),
       [
         '<section>',
-        '  <h1 id="test-lorem-ipsum-">Lorem ipsum 😪</h1>',
-        '  <h2 id="test-dolorsitamet">dolor—sit—amet</h2>',
-        '  <h3 id="test-consectetur--adipisicing">consectetur &#x26; adipisicing</h3>',
-        '  <h4 id="test-elit">elit</h4>',
-        '  <h5 id="test-elit-1">elit</h5>',
+        '  <h1 class="big-title test-h1">Lorem ipsum 😪</h1>',
+        '  <h2 class="test-h2">dolor—sit—amet</h2>',
+        '  <h3 class="test-h3">consectetur &#x26; adipisicing</h3>',
+        '  <h4 class="test-h4">elit</h4>',
+        '  <h5 class="test-h5">elit</h5>',
         '  <p>sed</p>',
         '</section>'
       ].join('\n')
